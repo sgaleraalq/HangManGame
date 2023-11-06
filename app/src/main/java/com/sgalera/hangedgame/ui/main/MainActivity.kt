3e5855a1db0571fc270e52faa4035575e283735f
sgaleraalq <sgaleraalq@gmail.com>
2023-11-06 22:37:20 +0100

    Bottom navigation configured

--- app/src/main/java/com/sgalera/hangedgame/ui/main/MainActivity.kt
+++ app/src/main/java/com/sgalera/hangedgame/ui/main/MainActivity.kt
@@ -3,6 +3,9 @@ package com.sgalera.hangedgame.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.sgalera.hangedgame.R
 import com.sgalera.hangedgame.databinding.ActivityMainBinding
 
@@ -18,10 +21,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUI(){
+        navigationDefaultInit()
         initNavigation()
     }
 
-    private fun initNavigation(){
+    private fun navigationDefaultInit(){
+        val navigationView: BottomNavigationView = findViewById(R.id.navigationBar)
+        navigationView.selectedItemId = R.id.gameFragment
+    }
 
+    private fun initNavigation(){
+//        val navHost = supportFragmentManager.findFragmentById(R.id.navigationBar) as NavHostFragment
+//        navController = navHost.navController
+//        binding.bottomNavView.setupWithNavController(navController)
     }
 }
