0564ee844267b68dec752c5a3e4d1b3798ab96c6
sgaleraalq <sgaleraalq@gmail.com>
2023-11-06 19:42:49 +0100

    Initial commit

---
+++ app/src/main/java/com/sgalera/hangedgame/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sgalera.hangedgame.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sgalera.hangedgame.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
