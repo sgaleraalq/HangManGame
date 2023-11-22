d76b7f0db8b6d215bbc74e402f7beb80faf04ee9
sgaleraalq <sgaleraalq@gmail.com>
2023-11-22 20:34:36 +0100

    Log In Screen

--- app/src/main/java/com/sgalera/hangedgame/ui/user/UserFragment.kt
+++ app/src/main/java/com/sgalera/hangedgame/ui/user/UserFragment.kt
@@ -1,14 +1,22 @@
 package com.sgalera.hangedgame.ui.user
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.sgalera.hangedgame.R
 import com.sgalera.hangedgame.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
+
+    private lateinit var tvSignUp: TextView
+    private lateinit var ivNotSigned: ImageView
+
 
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
@@ -18,6 +26,23 @@ class UserFragment : Fragment() {
     ): View {
         _binding = FragmentUserBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI(){
+        initComponents()
+    }
+
+    private fun initComponents() {
+        tvSignUp = binding.tvSignUp
+        tvSignUp.paintFlags = tvSignUp.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        ivNotSigned = binding.ivNotSigned
+        ivNotSigned.setColorFilter(ContextCompat.getColor(this.requireContext(), R.color.white))
     }
 
 }
